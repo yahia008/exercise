2b84e180afeef5721f46de66e34c6c715159e8a8
yahia008 <yahyatijjani99@gmail.com>
2024-07-19 09:27:39 -0700

    first commit

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': '41b9baf5edmshe8da57e4de32f9ap1f082djsn543d93814799',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': '41b9baf5edmshe8da57e4de32f9ap1f082djsn543d93814799',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
